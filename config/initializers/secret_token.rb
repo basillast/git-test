fc2213ec42722795832fb89d84b251053a507e92
basillast <last@last-no-MacBook-Air.local>
2013-09-01 11:08:31 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sumire::Application.config.secret_token = '24789f59aad5e397fbeaf269110eecae5f0585981a2dd0d619fe73b02b36d8b9992eae0171eea4c5f520598cd58f0fc05fbf77ab75e733e24e26add93c6e846f'
